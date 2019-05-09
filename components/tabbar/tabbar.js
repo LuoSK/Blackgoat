dc61f79cba11a5a20bbc54c06b883bf10f54ab15
Mac LuoSK <unknown@example.com>
2019-05-09 19:36:53 +0800

    1

--- components/tabbar/tabbar.js
+++ components/tabbar/tabbar.js
@@ -2,8 +2,24 @@ Component({
   properties: {
     tabbar: {
       type: Object,
-      value:{
-        background 
+      value: {
+        "backgroundColor": "#ffffff",
+        "color": "#979795",
+        "selectedColor": "#db9019",
+        "list": [{
+            "pagePath": "pages/index/index",
+            "text": "时间轴"
+          },
+          {
+            "pagePath": "pages/add/add",
+            "text": "添加",
+            "isSpecial": true
+          },
+          {
+            "pagePath": "pages/me/me",
+            "text": "Me"
+          }
+        ]
       }
 
     }
