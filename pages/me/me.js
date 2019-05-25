b28d13c85e7a1d1d420e8edaa70a9c846f7f55bf
Mac LuoSK <unknown@example.com>
2019-05-25 17:41:37 +0800

    1

--- pages/me/me.js
+++ pages/me/me.js
@@ -3,7 +3,20 @@ Page({
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     tabbar: {},
-    userInfo: []
+    userInfo: [],
+    userRecord: [{
+      event: '任务总数',
+      data: '10'
+    }, {
+      event: '已完成',
+      data: '6'
+    }, {
+      event: '未完成',
+      data: '4'
+    }, {
+      event: '完成率',
+      data: '60%'
+    }]
   },
   onLoad: function() {
     app.editTabbar();
